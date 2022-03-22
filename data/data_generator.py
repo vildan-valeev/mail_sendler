0cf9f4af9a343341ba6868bd253a437064374db9
vildan-valeev <vildan.maydanovich.valeev@gmail.com>
2022-03-22 23:24:01 +0500

    create data generator

---
+++ data/data_generator.py
@@ -0,0 +1,28 @@
+import csv
+import string
+import time
+from datetime import datetime
+from random import randint, choice
+
+count_rows = 500000
+header = ['first_name', 'last_name', 'b_date', 'email']
+
+t0 = time.time()
+outfile = 'csv_file.csv'
+
+
+def generate_row():
+    r = '%s,%s,%s,%s\n' % (
+        ''.join(choice(string.ascii_lowercase) for _ in range(10)),
+        ''.join(choice(string.ascii_lowercase) for _ in range(10)),
+        datetime.fromtimestamp(randint(1, int(time.time()))).strftime('%Y-%m-%d %H:%M:00'),
+        ''.join(choice(string.ascii_lowercase) for _ in range(5)) + choice(['@mail.ru', '@gmail.com', '@yandex.ru'])
+    )
+    return r
+
+
+with open(outfile, 'w') as f:
+    rows = [generate_row() for row in range(count_rows)]
+    f.writelines(rows)
+tdelta = time.time() - t0
+print(tdelta)
